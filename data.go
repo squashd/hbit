de91a162067124eb3ad78bfc7313b4c27d67037f
Daniel Hjartland <squashical@gmail.com>
2024-04-04 11:39:05 +0200

    (event)feat: create UUIDs for various events

--- data.go
+++ data.go
@@ -1,7 +1,21 @@
 package hbit
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func NewUUID() string {
 	return uuid.New().String()
+}
+
+func NewEventId(domain string) string {
+	return domain + "-" + NewUUID()
+}
+
+func NewEventIdWithTimestamp(domain string) string {
+	timestamp := time.Now().UTC().Format("20060102-150405")
+	return fmt.Sprintf("%s-%s-%s", domain, timestamp, NewUUID())
 }
