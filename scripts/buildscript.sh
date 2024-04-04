c99d1b904f317990d25d13e1ee6a1926f5dc17d1
Daniel Hjartland <squashical@gmail.com>
2024-04-04 03:59:11 +0200

    chore: move scripts to scripts directory

--- scripts/buildscript.sh
+++ scripts/buildscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/apiGateway ./cmd/api-gateway 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/taskSvc ./cmd/task-service 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/rpgSvc ./cmd/rpg-service 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/updateSvc ./cmd/update-service 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/featSvc ./cmd/feat-service 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/userSvc ./cmd/user-service 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/authSvc ./cmd/auth-service 
